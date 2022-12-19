afc80ae66167ff84350d8ed9869a82834b1eb4bb
Serhii Chupryna <schupryna@pypestream.com>
2022-12-19 14:49:22 +0100

    review added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,9 @@
-import { Todo } from './Todo'
-import { todoMachine } from 'xstate/todoMachine'
-import { useMachine } from '@xstate/react'
+import { TodoApp } from './Todo'
 
 function App() {
-  const [state, send] = useMachine(todoMachine)
-  const { todos, id, text } = state.context
-
   return (
     <div className="bg-white">
-      <Todo
-        todos={todos}
-        toggleTodo={(completed) => send({ type: 'TOGGLE', completed })}
-        addTodo={(text) => send('ADD_TODO', { text })}
-        onChangeTodo={(text) => send({ type: 'ON_CHANGE', text })}
-        text={text as string}
-        id={id as number}
-        deleteTodo={(todo) => send({ type: 'DELETE', todo })}
-      />
+      <TodoApp />
     </div>
   )
 }
